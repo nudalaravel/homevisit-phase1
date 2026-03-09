10e6895ba41a57f997498d30029f5445ede01b82
nuda_kullasom <kulsom.amat@gmail.com>
2026-03-09 09:27:14 +0700

    bf

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -4,7 +4,6 @@ import { normalizeURL, decode } from 'ufo'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-<<<<<<< HEAD
 const _11942510 = () => interopDefault(import('..\\pages\\admin-document.vue' /* webpackChunkName: "pages/admin-document" */))
 const _228a85b9 = () => interopDefault(import('..\\pages\\admin-payment.vue' /* webpackChunkName: "pages/admin-payment" */))
 const _1a313e00 = () => interopDefault(import('..\\pages\\admin-payment-history.vue' /* webpackChunkName: "pages/admin-payment-history" */))
@@ -16,24 +15,8 @@ const _8142d464 = () => interopDefault(import('..\\pages\\supervisor-dashboard.v
 const _0a7b3f51 = () => interopDefault(import('..\\pages\\supervisor-document.vue' /* webpackChunkName: "pages/supervisor-document" */))
 const _7d02be6a = () => interopDefault(import('..\\pages\\supervisor-report.vue' /* webpackChunkName: "pages/supervisor-report" */))
 const _3c8ee4e0 = () => interopDefault(import('..\\pages\\supervisor-survey.vue' /* webpackChunkName: "pages/supervisor-survey" */))
-const _e3e37d62 = () => interopDefault(import('..\\pages\\supervisor-survey-backup.vue' /* webpackChunkName: "pages/supervisor-survey-backup" */))
 const _adc26d0e = () => interopDefault(import('..\\pages\\survey.vue' /* webpackChunkName: "pages/survey" */))
 const _10d36dfa = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
-=======
-const _2b2e6090 = () => interopDefault(import('../pages/admin-document.vue' /* webpackChunkName: "pages/admin-document" */))
-const _2f5b490e = () => interopDefault(import('../pages/admin-payment.vue' /* webpackChunkName: "pages/admin-payment" */))
-const _255d76c0 = () => interopDefault(import('../pages/admin-payment-history.vue' /* webpackChunkName: "pages/admin-payment-history" */))
-const _854d9aaa = () => interopDefault(import('../pages/admin-receipt.vue' /* webpackChunkName: "pages/admin-receipt" */))
-const _3be2b8b2 = () => interopDefault(import('../pages/admin-visit-results.vue' /* webpackChunkName: "pages/admin-visit-results" */))
-const _681781da = () => interopDefault(import('../pages/login.vue' /* webpackChunkName: "pages/login" */))
-const _7177685a = () => interopDefault(import('../pages/supervisor-booking.vue' /* webpackChunkName: "pages/supervisor-booking" */))
-const _7eeec80e = () => interopDefault(import('../pages/supervisor-dashboard.vue' /* webpackChunkName: "pages/supervisor-dashboard" */))
-const _5afbc511 = () => interopDefault(import('../pages/supervisor-document.vue' /* webpackChunkName: "pages/supervisor-document" */))
-const _692c97ac = () => interopDefault(import('../pages/supervisor-report.vue' /* webpackChunkName: "pages/supervisor-report" */))
-const _301f8350 = () => interopDefault(import('../pages/supervisor-survey.vue' /* webpackChunkName: "pages/supervisor-survey" */))
-const _4510288e = () => interopDefault(import('../pages/survey.vue' /* webpackChunkName: "pages/survey" */))
-const _c320427a = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
->>>>>>> ea9ff961b8ede665b155913fb4d85c2a88797194
 
 const emptyFn = () => {}
 
@@ -91,13 +74,6 @@ export const routerOptions = {
     component: _3c8ee4e0,
     name: "supervisor-survey"
   }, {
-<<<<<<< HEAD
-    path: "/supervisor-survey-backup",
-    component: _e3e37d62,
-    name: "supervisor-survey-backup"
-  }, {
-=======
->>>>>>> ea9ff961b8ede665b155913fb4d85c2a88797194
     path: "/survey",
     component: _adc26d0e,
     name: "survey"
